8cb3637affd2d5b238da9226a6fce50666a31460
t-sakurai816 <poppun_0816@icloud.com>
2021-03-01 21:01:46 +0900

    動画リストを表示

--- lib/routes/home/home_main.dart
+++ lib/routes/home/home_main.dart
@@ -4,13 +4,85 @@ import '../../header/header_main.dart';
 
 class HomeMain extends StatelessWidget {
   final String bodyName = 'Home'; //headerに表示される名前
+
+  final Map _movieData = {
+    'Icon':
+        'https://pbs.twimg.com/profile_images/1356263946638950410/s72cF6s2.jpg',
+    'Title': '初めての動画投稿',
+    'Thumbnail':
+        'https://s1.1zoom.me/b5050/596/Evening_Forests_Mountains_Firewatch_Campo_Santo_549147_1920x1080.jpg',
+    'Name': 'Takumi Sakurai',
+    'Views': '2.3万回',
+    'timestamp': '2時間前'
+  };
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HeaderMain(),
-      body: Center(
-        child: Text(bodyName),
+      body: ListView.builder(
+        // 10回リストを表示する
+        itemBuilder: (BuildContext context, int index) {
+          return PostMovieList(movieData: _movieData);
+        },
+        itemCount: 10,
       ),
+    );
+  }
+}
+
+class PostMovieList extends StatelessWidget {
+  const PostMovieList({
+    Key key,
+    @required Map movieData,
+  })  : _movieData = movieData,
+        super(key: key);
+
+  final Map _movieData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // 16:9のサムネを表示
+        Image.network(_movieData['Thumbnail']),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            child: Row(
+              children: [
+                Container(
+                  height: 50,
+                  child: ClipRRect(
+                    // アイコンを丸くする
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image.network(_movieData['Icon']),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start, // 左寄せ
+                    children: [
+                      Text(
+                        _movieData['Title'],
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        softWrap: true,
+                      ),
+                      Row(
+                        children: [
+                          Text(_movieData['Name']),
+                          Text('・' + _movieData['Views']),
+                          Text('・' + _movieData['timestamp']),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
